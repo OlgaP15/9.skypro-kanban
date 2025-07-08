22058b605a723ddb2a2d64313cddb92592cf9e9e
Olga P. <zjkz14@gmail.com>
2025-07-09 00:50:33 +0300

    разбор App-компонента на несколько компонентов.

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Column from "../Column/Column";
+
+export default function Main() {
+  const statuses = [
+    "Без статуса",
+    "Нужно сделать",
+    "В работе",
+    "Тестирование",
+    "Готово",
+  ];
+
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="main__block">
+          {statuses.map((status) => (
+            <Column key={status} title={status} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
